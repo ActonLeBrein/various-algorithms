29e992ac71ebb6ab624931919eec890fe1ea1405
Acton <actonlebrein@gmail.com>
2018-02-12 23:58:13 -0600

    Update DepthFirstSearch.py

--- DepthFirstSearch.py
+++ DepthFirstSearch.py
@@ -29,3 +29,19 @@ def DFS(g,s,e,p):
             i += 1
 
 DFS(graph,'A','D',[])
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        print path
+    if not graph.has_key(start):
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+find_all_paths(graph,'A','D')
