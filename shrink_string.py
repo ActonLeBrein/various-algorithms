a4caa8d7b7a6f2c61159707e16b46abdbb70c2ee
Acton <actonlebrein@gmail.com>
2018-02-18 12:47:31 -0600

    Update shrink_string.py

--- shrink_string.py
+++ shrink_string.py
@@ -1,4 +1,4 @@
-def shrink_string(s):
+def shrink_string_iterative(s):
   counter=1
   cycle=0
   len_s=0
@@ -35,4 +35,28 @@ def shrink_string(s):
     if cycle==2:
       flag=False
   print s
-shrink_string('abaabbaaab')
+shrink_string_iterative('abaabbaaab')
+
+def shrink_string_recursive(string):
+  flag=False
+  j=0
+  for i in xrange(len(string)-1):
+    while string[i]==string[i+j] and j+i!=len(string)-1:
+      j+=1
+    if j>=3:
+      if j+i==len(string)-1:
+        print string[:i]
+        string=string[:i]
+      else:
+        print string[:i],string[j+i:]
+        string=string[:i]+string[j+i:]
+      flag=True
+      break
+    else:
+      j=0
+  if flag:
+    shrink_string_recursive(string)
+  else:
+    print string
+    
+shrink_string_recursive('aaaaabcccccdeeeffffgggg')
